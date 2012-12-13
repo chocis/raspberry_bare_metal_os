fc8b3c3a0bbbd1a0e020a9e73d20943c8a40fb62
chocis <janis.coders@gmail.com>
2012-12-13 21:29:33 +0200

    updated upload bootloader script, started implementing interrupted uart

--- source/START.c
+++ source/START.c
@@ -16,7 +16,7 @@ void notmain ( void )
 
     DEBUG("Hey debug %d works? A: %s, hex: %x\r\n", 55, "YOO", 100);
 
-    print_all_atags();
+//    print_all_atags();
 
 
 
@@ -34,5 +34,25 @@ void notmain ( void )
 //    }
 
     while(1){}
+
+}
+
+void c_irq_handler ( void )
+{
+//    unsigned int rb,rc;
+//
+//    //an interrupt has occurred, find out why
+//    while(1) //resolve all interrupts to uart
+//    {
+//        rb=GET32(AUX_MU_IIR_REG);
+//        if((rb&1)==1) break; //no more interrupts
+//        if((rb&6)==4)
+//        {
+//            //receiver holds a valid byte
+//            rc=GET32(AUX_MU_IO_REG); //read byte from rx fifo
+//            rxbuffer[rxhead]=rc&0xFF;
+//            rxhead=(rxhead+1)&RXBUFMASK;
+//        }
+//    }
 
 }
--- uploadviabootloader.sh
+++ uploadviabootloader.sh
@@ -1,24 +1,25 @@
 #!/usr/bin/expect -f
 
-set force_conservative 0  ;# set to 1 to force conservative mode even if
-
-if {$force_conservative} {
-	set send_slow {1 .1}
-	proc send {ignore arg} {
-		sleep .1
-		exp_send -s -- $arg
-	}
-}
 
 
-set timeout -1
+#spawns serial connection
 spawn cu -l /dev/ttyUSB0 -s 115200
-match_max 100000
+#wait when connected
 expect "Connected"
+
+#send typical ~ cu command, then wait for answer (mine is [chocis-PC]), thats why using only "]"
 send -- "~"
-expect "chocis-PC"
+expect "]"
+
+#then send + cu command, which allows to insert normal shell commands (wait for + answer or else it wont work)
 send -- "+"
 expect  "+"
+
+#send image via modemx protocol
 send -- "sx -X kernel.img\r"
-expect "UART"
-expect eof
+
+#give control back to user, so I can send commands like in normal cu usage
+interact
+
+#because (I think so) expect is running in different process, cu cant receive ~. command to exit connection,
+#thats why you must press CTRL+C and only then ~. will work.
