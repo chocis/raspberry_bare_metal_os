14f33a379fc061929336d89b599f7ababf2bd58c
chocis <janis.coders@gmail.com>
2012-12-14 17:44:34 +0200

    added for real

---
+++ source/headers/ringBuffer.h
@@ -0,0 +1,32 @@
+#ifndef RINGBUFFER_H_
+#define RINGBUFFER_H_
+
+#include "types.h"
+
+/* TODO
+* For now these values are global. Should implement using struct.
+* Maybe use MASKED buffer which gives much better permormance.
+* It must use uffer size of power 2, and then can extra values be masked out by simple &.
+* That allows to use many features without if statements. See details:
+* http://stackoverflow.com/questions/9718116/improving-c-circular-buffer-efficiency
+*/
+
+
+//remember that max element count is size-1, so that indexes do not overlap and start from beginning
+#define RING_BUFFER_SIZE 10
+
+//STATIC makes these variables visible only in this file
+static volatile char ringCurrentlyInBuffer[RING_BUFFER_SIZE]; //TODO - is this a good idea - to hold two buffers in memory?
+static volatile char ringBuffer[RING_BUFFER_SIZE];
+volatile u32 ringHeadIndex;
+volatile u32 ringTailIndex;
+volatile u32 ringSize;
+volatile u32 ringCount;
+
+u32 ringInit();
+u32 ringAddCharToBuffer(char newChar);
+u32 ringGetNextIndex(u32 index);
+volatile char* ringGetCurrentBufferString();
+void removeFirstBufferElements(u32 count);
+
+#endif /* RINGBUFFER_H_ */
---
+++ source/ringBuffer.c
@@ -0,0 +1,88 @@
+/*
+ * ringBuffer.c
+ *
+ * Created: 2012.10.08. 20:59:35
+ *  Author: chocis
+ *
+ *	Used for manupilating ring buffer array.
+ */
+
+#include "ringBuffer.h"
+
+
+void removeFirstBufferElements(u32 count){
+    u32 i;
+	for(i=0; i< count; i++){
+		ringTailIndex = ringGetNextIndex(ringTailIndex);
+		ringCount--;
+	}
+}
+
+u32 ringInit()
+{
+	ringHeadIndex = 0;
+	ringTailIndex = 0;
+	ringSize = 0;
+	ringCount = 0;
+	u32 success = 0;
+	if (RING_BUFFER_SIZE >= 2)
+	{
+		ringSize = RING_BUFFER_SIZE;
+		success = 1;
+	}
+	return success;
+}
+
+
+u32 ringGetNextIndex(u32 index){
+
+	if(index == ringSize-1){ //if index is already at
+		return 0; //next head index should start from 0
+	}
+	else{
+		return index+1; //else return next probable index
+	}
+}
+
+u32 ringAddCharToBuffer(char newChar){
+
+	u32 success = 0;
+	u32 nextProbableHeadIndex = ringGetNextIndex(ringHeadIndex);
+
+
+	if(!(nextProbableHeadIndex == ringTailIndex)){ //add char only if head will not overflow tail
+
+		ringBuffer[ringHeadIndex] = newChar;  //add new char to buffer
+		ringHeadIndex = nextProbableHeadIndex; //now its safe to assign index
+		ringCount++; //increase char count size inside buffer
+		success = 1;
+	}
+
+	return success;
+}
+
+volatile char* ringGetCurrentBufferString(){
+
+
+	if(ringHeadIndex == ringTailIndex){  //there are no elements in buffer
+		ringCurrentlyInBuffer[0] = '\0';
+	}
+	else{
+		u32 currentTailIndexTemp = ringTailIndex;
+		u32 counter = 0;
+
+		while (currentTailIndexTemp != ringHeadIndex)
+		{
+			ringCurrentlyInBuffer[counter] = ringBuffer[currentTailIndexTemp]; //take char from buffer
+			counter++;
+			currentTailIndexTemp = ringGetNextIndex(currentTailIndexTemp);
+		}
+
+		ringCurrentlyInBuffer[counter] = '\0';
+
+
+	}
+	return ringCurrentlyInBuffer;
+}
+
+
